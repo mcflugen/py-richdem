74bc625c89b49d8ddb9d6721be6270766537ba41
mcflugen <mcflugen@gmail.com>
2026-02-25 16:19:10 -0700

    refactor setup.py

--- setup.py
+++ setup.py
@@ -3,55 +3,56 @@ import sys
 
 from pybind11.setup_helpers import Pybind11Extension
 from setuptools import setup
-from setuptools.command.build_ext import build_ext as _build_ext
 
-richdem_prefix = os.environ.get("RICHDEM_PREFIX", sys.prefix)
-openmp_prefix = os.environ.get("OPENMP_PREFIX", sys.prefix)
+richdem_prefix = os.environ.get("RICHDEM_PREFIX")
+openmp_prefix = os.environ.get("OPENMP_PREFIX")
 
-# Compiler specific arguments
-BUILD_ARGS = {
-    "msvc": ["-std=c++17", "-g", "-fvisibility=hidden", "-O3"],
-    "gcc": ["-std=c++17", "-g", "-fvisibility=hidden", "-O3"],
-    "unix": ["-std=c++17", "-g", "-fvisibility=hidden", "-O3"],
-}
+sources: list[str] = ["cpp/pywrapper.cpp"]
 
+include_dirs: list[str] = []
+library_dirs: list[str] = []
+extra_objects: list[str] = []
+libraries: list[str] = []
+extra_link_args: list[str] = []
+extra_compile_args: list[str] = ["-fvisibility=hidden", "-O3"]
+define_macros: list[str, str | None] = [("DOCTEST_CONFIG_DISABLE", None)]
 
-# Magic that hooks compiler specific arguments up with the compiler
-class build_ext_compiler_check(_build_ext):
-    def build_extensions(self):
-        compiler = self.compiler.compiler_type
-        print(f"COMPILER {compiler}")
-        args = BUILD_ARGS[compiler]
-        for ext in self.extensions:
-            ext.extra_compile_args = args
-            print(f"COMPILER ARGUMENTS: {ext.extra_compile_args}")
-        _build_ext.build_extensions(self)
+if richdem_prefix:
+    include_dirs.append(os.path.join(richdem_prefix, "include"))
+    library_dirs.append(os.path.join(richdem_prefix, "lib"))
+    extra_objects.append(os.path.join(richdem_prefix, "lib", "librichdem.a"))
+else:
+    extra_objects.append("librichdem.a")
 
+if openmp_prefix:
+    include_dirs.append(os.path.join(openmp_prefix, "include"))
+    library_dirs.append(os.path.join(openmp_prefix, "lib"))
+
+    if sys.platform == "darwin":
+        extra_compile_args += ["-Xpreprocessor", "-fopenmp"]
+        libraries.append("omp")
+
+if sys.platform == "linux":
+    extra_compile_args.append("-fopenmp")
+    extra_link_args.append("-fopenmp")
+
+if sys.platform.startswith("win"):
+    define_macros.append(("_USE_MATH_DEFINES", None))
 
 ext_modules = [
     Pybind11Extension(
         "_richdem",
-        ["cpp/pywrapper.cpp"],
-        include_dirs=[
-            os.path.join(richdem_prefix, "include"),
-            os.path.join(openmp_prefix, "include"),
-        ],
-        library_dirs=[
-            os.path.join(richdem_prefix, "lib"),
-            os.path.join(openmp_prefix, "lib"),
-        ],
-        libraries=["omp"],
-        extra_objects=[os.path.join(richdem_prefix, "lib", "librichdem.a")],
-        define_macros=[
-            ("DOCTEST_CONFIG_DISABLE", None),
-            ("RICHDEM_LOGGING", None),
-            ("_USE_MATH_DEFINES", None),
-        ],
+        sources,
+        include_dirs=include_dirs,
+        library_dirs=library_dirs,
+        libraries=libraries,
+        extra_compile_args=extra_compile_args,
+        extra_link_args=extra_link_args,
+        extra_objects=extra_objects,
+        cxx_std=17,
+        define_macros=define_macros,
     ),
 ]
 
 
-setup(
-    ext_modules=ext_modules,
-    cmdclass={"build_ext": build_ext_compiler_check},
-)
+setup(ext_modules=ext_modules)
