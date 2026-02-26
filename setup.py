3f26d0acc3ed27013a917a1021de1173eabf4e4e
mcflugen <mcflugen@gmail.com>
2026-02-26 15:49:44 -0700

    add Windows wheels to the ci builds
    * add cibuildwheel windows section to pyproject.toml
    * add windows-specific compile flags in setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ import sys
 from pybind11.setup_helpers import Pybind11Extension
 from setuptools import setup
 
+is_windows = sys.platform.startswith("win")
+is_macos = sys.platform == "darwin"
+is_linux = sys.platform.startswith("linux")
+
 richdem_prefix = os.environ.get("RICHDEM_PREFIX")
 openmp_prefix = os.environ.get("OPENMP_PREFIX")
 
@@ -14,30 +18,40 @@ library_dirs: list[str] = []
 extra_objects: list[str] = []
 libraries: list[str] = []
 extra_link_args: list[str] = []
-extra_compile_args: list[str] = ["-fvisibility=hidden", "-O3"]
-define_macros: list[str, str | None] = [("DOCTEST_CONFIG_DISABLE", None)]
+extra_compile_args: list[str] = []
+define_macros: list[str, str | None] = []
 
 if richdem_prefix:
     include_dirs.append(os.path.join(richdem_prefix, "include"))
     library_dirs.append(os.path.join(richdem_prefix, "lib"))
-    extra_objects.append(os.path.join(richdem_prefix, "lib", "librichdem.a"))
+    if is_windows:
+        libraries.append("richdem")
+    else:
+        extra_objects.append(os.path.join(richdem_prefix, "lib", "librichdem.a"))
 else:
-    extra_objects.append("librichdem.a")
+    if is_windows:
+        libraries.append("richdem")
+    else:
+        extra_objects.append("librichdem.a")
 
 if openmp_prefix:
     include_dirs.append(os.path.join(openmp_prefix, "include"))
     library_dirs.append(os.path.join(openmp_prefix, "lib"))
 
-    if sys.platform == "darwin":
-        extra_compile_args += ["-Xpreprocessor", "-fopenmp"]
-        libraries.append("omp")
-
-if sys.platform == "linux":
+if is_linux:
     extra_compile_args.append("-fopenmp")
     extra_link_args.append("-fopenmp")
 
-if sys.platform.startswith("win"):
+if is_macos:
+    extra_compile_args += ["-Xpreprocessor", "-fopenmp"]
+    libraries.append("omp")
+    if richdem_prefix:
+        extra_objects.append(os.path.join(richdem_prefix, "lib", "librichdem.a"))
+
+if is_windows:
     define_macros.append(("_USE_MATH_DEFINES", None))
+    extra_compile_args = ["/O2"]
+
 
 ext_modules = [
     Pybind11Extension(
